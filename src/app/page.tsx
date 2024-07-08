fc022397ef4500df6569053bec6ff8e39b9c39cd
송경호 <s07019@naver.com>
2024-07-08 18:35:42 +0900

    Chore: vanilla-extract installation & setting part of SWM-45 related to #2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { testingVanilla } from '@/app/style.css';
+
 export default function Home() {
-  return <h1>Testing Prettier</h1>;
+  return <h1 className={testingVanilla}>Testing Prettier</h1>;
 }
---
+++ src/app/style.css.ts
@@ -0,0 +1,6 @@
+import { style } from '@vanilla-extract/css';
+
+export const testingVanilla = style({
+  backgroundColor: 'blue',
+  color: 'white',
+});
