cfd8ffb3743885503e3d48b0362b975e58929951
송경호 <s07019@naver.com>
2024-07-08 18:54:44 +0900

    Chore: zustand installation part of SWM-45 related to #2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+'use client';
+
 import { testingVanilla } from '@/app/style.css';
+import BearCounter from '@/app/zustand_test';
 
 export default function Home() {
-  return <h1 className={testingVanilla}>Testing Prettier</h1>;
+  return (
+    <>
+      <h1 className={testingVanilla}>Testing Prettier</h1>
+      <BearCounter />
+    </>
+  );
 }
