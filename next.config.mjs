91019e4c98b9daa1f5d95427d94158329c56006f
송경호 <s07019@naver.com>
2024-07-11 14:35:40 +0900

    Fix: eliminate remaining vanilla-extract setting code in next.config.mjs part of SWM-52 related to #5

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
-import {createVanillaExtractPlugin} from "@vanilla-extract/next-plugin";
-
-const withVanillaExtract = createVanillaExtractPlugin();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withVanillaExtract(nextConfig);
+export default nextConfig;
