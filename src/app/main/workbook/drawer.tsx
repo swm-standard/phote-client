cef97dca7283854455d6e144c31efe0bbc01221b
송경호 <s07019@naver.com>
2024-07-11 16:16:24 +0900

    Chore: install shadcn/ui and test Drawer component part of SWM-71 related to #9

---
+++ src/app/main/workbook/drawer.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
+
+export function DrawerDemo() {
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>
+        <button>Open Drawer</button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="mx-auto w-full max-w-sm">
+          <DrawerHeader>
+            <DrawerTitle>Move Goal</DrawerTitle>
+            <DrawerDescription>Set your daily activity goal.</DrawerDescription>
+          </DrawerHeader>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+}
