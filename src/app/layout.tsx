d8d69a7ff60f57d8bdf716a53af0581412dd6ecd
송경호 <s07019@naver.com>
2024-07-11 12:58:26 +0900

    Chore: install TailwindCSS and initialize settings part of SWM-69 related to #6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { screen } from './app.css';
+import './global.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
--- src/app/main/workbook/page.tsx
+++ src/app/main/workbook/page.tsx
@@ -5,7 +5,7 @@ import WorkbookArea from '@/app/main/workbook/workbook-area';
 const Page = () => {
   return (
     <div>
-      <h1>문제집 관리</h1>
+      <h1 className="text-3xl font-bold underline">문제집 관리</h1>
       <WorkbookArea />
     </div>
   );
