355752dc9e72b3e6d763cab240573c71f77a4233
송경호 <s07019@naver.com>
2024-07-14 21:52:16 +0900

    Feat: add Question and QuestionInWorkbook type
    part of SWM-77
    related to #11

--- src/app/types.ts
+++ src/app/types.ts
@@ -6,3 +6,20 @@ export type Workbook = {
   quantity: number;
   modifiedAt: Date;
 };
+
+export type Question = {
+  id: string;
+  status: string;
+  options: string;
+  image: string;
+  answer: string;
+  category: string;
+  tags: string[];
+  modifiedAt: Date;
+};
+
+export type QuestionInWorkbook = {
+  sequence: number;
+} & Question;
+
+export type Status = 'loading' | 'error' | 'success';
