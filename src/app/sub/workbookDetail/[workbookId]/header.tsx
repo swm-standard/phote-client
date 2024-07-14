dc6ed05351396d5eb3f571cb390fc771ace7c855
송경호 <s07019@naver.com>
2024-07-14 22:58:11 +0900

    Feat: implement navigation to the previous page when the back button is pressed part of SWM-77 related to #11

--- src/app/sub/workbookDetail/[workbookId]/header.tsx
+++ src/app/sub/workbookDetail/[workbookId]/header.tsx
@@ -1,9 +1,17 @@
+'use client';
 import React from 'react';
+import { useRouter } from 'next/navigation';
 
 const Header = () => {
+  const router = useRouter();
+
+  const handleBackClick: React.MouseEventHandler<HTMLButtonElement> = () => {
+    router.back();
+  };
+
   return (
     <div className="flex flex-row">
-      <button>go back</button>
+      <button onClick={handleBackClick}>go back</button>
       <h1>문제집 상세</h1>
     </div>
   );
