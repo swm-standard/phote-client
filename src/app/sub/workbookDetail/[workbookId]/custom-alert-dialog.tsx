95c882ce02476bd253b29038ecfdb559ce89c4e9
송경호 <s07019@naver.com>
2024-07-15 15:43:58 +0900

    Feat: install shadcn/ui alert dialog and test a simple UI part of SWM-82 related to #13

---
+++ src/app/sub/workbookDetail/[workbookId]/custom-alert-dialog.tsx
@@ -0,0 +1,35 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
+
+export function AlertDialogDemo({ isOpen }: { isOpen?: boolean }) {
+  return (
+    <AlertDialog open={isOpen}>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline">Show Dialog</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent className="bg-white w-4/5 rounded-md">
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
--- src/app/sub/workbookDetail/[workbookId]/page.tsx
+++ src/app/sub/workbookDetail/[workbookId]/page.tsx
@@ -5,6 +5,7 @@ import Header from '@/app/sub/workbookDetail/[workbookId]/header';
 import WorkbookInformation from '@/app/sub/workbookDetail/[workbookId]/workbook-information';
 import WorkbookEditButtons from '@/app/sub/workbookDetail/[workbookId]/workbook-edit-buttons';
 import WorkbookQuestions from '@/app/sub/workbookDetail/[workbookId]/workbook-questions';
+import { AlertDialogDemo } from '@/app/sub/workbookDetail/[workbookId]/custom-alert-dialog';
 
 const Page = () => {
   const params = useParams<{ workbookId: string }>();
@@ -14,6 +15,7 @@ const Page = () => {
       <Header />
       <WorkbookInformation />
       <WorkbookEditButtons />
+      <AlertDialogDemo />
       <WorkbookQuestions />
     </div>
   );
