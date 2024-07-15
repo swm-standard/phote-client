b7af130703df4bd73b32a4ffd18a1f7e4a304b72
송경호 <s07019@naver.com>
2024-07-15 17:14:45 +0900

    Feat: connect delete dialog to delete workbook button part of SWM-82 related to #13

--- src/app/sub/workbookDetail/[workbookId]/page.tsx
+++ src/app/sub/workbookDetail/[workbookId]/page.tsx
@@ -5,7 +5,6 @@ import Header from '@/app/sub/workbookDetail/[workbookId]/header';
 import WorkbookInformation from '@/app/sub/workbookDetail/[workbookId]/workbook-information';
 import WorkbookEditButtons from '@/app/sub/workbookDetail/[workbookId]/workbook-edit-buttons';
 import WorkbookQuestions from '@/app/sub/workbookDetail/[workbookId]/workbook-questions';
-import { AlertDialogDemo } from '@/app/sub/workbookDetail/[workbookId]/custom-alert-dialog';
 
 const Page = () => {
   const params = useParams<{ workbookId: string }>();
@@ -15,7 +14,6 @@ const Page = () => {
       <Header />
       <WorkbookInformation />
       <WorkbookEditButtons />
-      <AlertDialogDemo />
       <WorkbookQuestions />
     </div>
   );
