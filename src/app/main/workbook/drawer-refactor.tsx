791d0df747854575c2d08be097a74109614e9621
송경호 <s07019@naver.com>
2024-07-16 01:21:13 +0900

    Refactor: create a new drawer that uses the refactored text input component part of SWM-83 related to #15

---
+++ src/app/main/workbook/drawer-refactor.tsx
@@ -0,0 +1,76 @@
+'use client';
+
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
+
+import { Label } from '@/components/ui/label';
+import { TextInput } from '@/components/custom/text-input';
+import React, { useRef, useState } from 'react';
+import { createWorkbookUrl } from '@/app/endpoint';
+
+function DrawerRefactor() {
+  const [open, setOpen] = useState<boolean>(false);
+  const [response, setResponse] = useState<string>('');
+  const titleRef = useRef<HTMLInputElement>(null);
+  const descriptionRef = useRef<HTMLInputElement>(null);
+
+  const handleToggleClick: React.MouseEventHandler<HTMLButtonElement> = () => {
+    setOpen(!open);
+  };
+
+  const handleFormSubmit: React.FormEventHandler<HTMLFormElement> = async (
+    e,
+  ) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch(createWorkbookUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title: titleRef.current?.value,
+          description: descriptionRef.current?.value,
+        }),
+      });
+
+      const data = await res.json();
+      setResponse('성공');
+      setOpen(false);
+    } catch (e) {
+      setResponse('실패!');
+      console.error('createWorkbook failed');
+    }
+  };
+
+  return (
+    <Drawer open={open}>
+      <DrawerTrigger asChild onClick={handleToggleClick}>
+        <button>Open Drawer</button>
+      </DrawerTrigger>
+
+      <DrawerContent className="bg-white">
+        <DrawerTitle>문제집 생성</DrawerTitle>
+        <DrawerDescription />
+        <DrawerClose onClick={handleToggleClick}>취소</DrawerClose>
+        {response}
+        <form onSubmit={handleFormSubmit}>
+          <Label htmlFor="title">문제집 제목</Label>
+          <TextInput id="title" allowClear ref={titleRef} />
+          <Label htmlFor="description">문제집 설명</Label>
+          <TextInput id="description" allowClear ref={descriptionRef} />
+          <button>확인</button>
+        </form>
+      </DrawerContent>
+    </Drawer>
+  );
+}
+
+export default DrawerRefactor;
