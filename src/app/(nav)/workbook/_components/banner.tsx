e25c1aaab5ade8f24ed363c92ebda5cce8a35050
송경호 <s07019@naver.com>
2024-07-24 00:56:53 +0900

    Design: implement banner part of SWM-104 related to #30

---
+++ src/app/(nav)/workbook/_components/banner.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import BannerIcon from '@/static/icons/banner-icon';
+
+const Banner = () => {
+  return (
+    <div className="bg-banner-bg w-full px-6 py-6 rounded-[20px] flex justify-between items-center">
+      <div className="flex flex-col gap-2.5">
+        <p className="font-semibold text-lg text-text-001">
+          함께 포테를 만들어나가요!
+        </p>
+        <button className="bg-white text-text-001 text-sm px-2 py-1 rounded-lg w-fit font-semibold">
+          건의하기
+        </button>
+      </div>
+      <BannerIcon />
+    </div>
+  );
+};
+
+export default Banner;
