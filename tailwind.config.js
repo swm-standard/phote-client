c4d947ddb7ce25c1c675164f3dd01d1aa4be34a6
송경호 <s07019@naver.com>
2024-07-24 00:21:18 +0900

    Chore: add brand colors on tailwind.config.js part of SWM-104 related to #30

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,23 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'outer-bg': '#444',
+        'app-bg': '#F7FBFF',
+        'text' : {
+          '001' : "#353542",
+          '002' : "#65656e",
+        },
+        'brand-blue' : {
+          'light' : "#6CD5E3",
+          'heavy' : '#39C6DA',
+        }
+      },
+      backgroundImage: {
+        'banner-bg' : 'linear-gradient(95.84deg, #BAF2EC 0%, #BCE7FF 97.09%)',
+      }
+    },
   },
   plugins: [],
 };
