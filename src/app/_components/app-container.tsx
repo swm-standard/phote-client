1b9069dce9c1ccc7eb8e94f16bc08329d822708a
송경호 <s07019@naver.com>
2024-07-24 00:34:57 +0900

    Design: modify root layout - add pretendard as default font - add AppContainer which is the container of the application content - max-w-[600px] set on AppContainer
    part of SWM-104
    related to #30

---
+++ src/app/_components/app-container.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppContainer = ({children} : {children : React.ReactNode}) => {
+    return (
+        <div className='mx-auto max-w-[600px] h-dvh bg-app-bg'>
+            {children}
+        </div>
+    );
+};
+
+export default AppContainer;
