e7738a469d8481d5a77304b6d225dab2b6d8cd11
송경호 <s07019@naver.com>
2024-07-25 15:33:50 +0900

    Feat: modify progress change footer to have appropriate button texts
    part of SWM-103
    related to #29

--- src/app/(top)/createQuestion/_components/progress/progress-change-footer.tsx
+++ src/app/(top)/createQuestion/_components/progress/progress-change-footer.tsx
@@ -1,31 +1,38 @@
 import React from 'react';
-import Step from '@/app/(top)/createQuestion/_components/progress/types';
+import { Step } from '@/app/(top)/createQuestion/_components/progress/types';
+
+const ButtonText = {
+  1: {
+    left: '직접 입력하기',
+    right: '사진 변환하기',
+  },
+  2: {
+    left: '사진 다시찍기',
+    right: '다음으로',
+  },
+  3: {
+    left: '이전으로',
+    right: '문제 생성',
+  },
+};
 
 const ProgressChangeFooter = ({
+  handleLeftButtonClick,
+  handleRightButtonClick,
   currentStep,
-  setToNextStep,
-  setToPrevStep,
 }: {
+  handleLeftButtonClick: () => void;
+  handleRightButtonClick: () => void;
   currentStep: Step;
-  setToNextStep: () => void;
-  setToPrevStep: () => void;
 }) => {
-  const handlePrevButtonClick: React.MouseEventHandler<
-    HTMLButtonElement
-  > = () => {
-    setToPrevStep();
-  };
-
-  const handleNextButtonClick: React.MouseEventHandler<
-    HTMLButtonElement
-  > = () => {
-    setToNextStep();
-  };
-
   return (
-    <div className="absolute bottom-0 left-0 w-full">
-      <button onClick={handlePrevButtonClick}>이전으로</button>
-      <button onClick={handleNextButtonClick}>다음으로</button>
+    <div className="w-full">
+      <button onClick={handleLeftButtonClick}>
+        {ButtonText[currentStep].left}
+      </button>
+      <button onClick={handleRightButtonClick}>
+        {ButtonText[currentStep].right}
+      </button>
     </div>
   );
 };
