051be1acbfe7b7e67e9f5803158e019bc0af5263
송경호 <s07019@naver.com>
2024-07-28 15:41:17 +0900

    Feat: change navigation logic to use useSelectedLayoutSegment hook - add appropriate icons
    part of SWM-109
    related to #32

--- src/app/(nav)/_components/navigation.tsx
+++ src/app/(nav)/_components/navigation.tsx
@@ -1,72 +1,65 @@
 'use client';
 
+import React from 'react';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { useSelectedLayoutSegment } from 'next/navigation';
 import WorkbookIcon from '@/static/icons/workbook-icon';
+import QuestionIcon from '@/static/icons/question-icon';
+import ShareIcon from '@/static/icons/share-icon';
+import ProfileIcon from '@/static/icons/profile-icon';
 
 interface NavigationItem {
   path: string;
   buttonText: string;
+  icon: React.ComponentType<{ className: string }>;
 }
 
 const navigationList: NavigationItem[] = [
   {
-    path: '/workbook',
+    path: 'workbook',
     buttonText: '문제집',
+    icon: WorkbookIcon,
   },
   {
-    path: '/question',
+    path: 'question',
     buttonText: '문제',
+    icon: QuestionIcon,
   },
   {
-    path: '/share',
+    path: 'share',
     buttonText: '공유',
+    icon: ShareIcon,
   },
   {
-    path: '/my',
+    path: 'my',
     buttonText: '마이페이지',
+    icon: ProfileIcon,
   },
 ];
 
 const Navigation = () => {
-  const path = usePathname();
+  const segment = useSelectedLayoutSegment();
 
   return (
-    <ul className="flex flex-row justify-between w-full bg-white ">
-      <li className="px-9 py-3 gap-1 flex flex-col justify-center items-center">
-        <WorkbookIcon className="fill-text-001 w-6 h-6" />
-        <p className="text-text-001 text-xs">문제집</p>
-      </li>
-      <li className="px-9 py-3 gap-1 flex flex-col justify-center items-center">
-        <WorkbookIcon className="fill-text-001 w-6 h-6" />
-        <p className="text-text-001 text-xs">문제집</p>
-      </li>
-      <li className="px-9 py-3 gap-1 flex flex-col justify-center items-center">
-        <WorkbookIcon className="fill-text-001 w-6 h-6" />
-        <p className="text-text-001 text-xs">문제집</p>
-      </li>
-      <li className="px-9 py-3 gap-1 flex flex-col justify-center items-center">
-        <WorkbookIcon className="fill-text-001 w-6 h-6" />
-        <p className="text-text-001 text-xs">문제집</p>
-      </li>
-      {/*{navigationList.map((item, idx) => {*/}
-      {/*  const isSelected = path === item.path;*/}
-
-      {/*  if (isSelected)*/}
-      {/*    return (*/}
-      {/*      <li key={idx} className="text-green-500">*/}
-      {/*        {item.buttonText}*/}
-      {/*      </li>*/}
-      {/*    );*/}
-      {/*  else*/}
-      {/*    return (*/}
-      {/*      <li key={idx}>*/}
-      {/*        <Link key={idx} href={item.path}>*/}
-      {/*          {item.buttonText}*/}
-      {/*        </Link>*/}
-      {/*      </li>*/}
-      {/*    );*/}
-      {/*})}*/}
+    <ul className="flex w-full flex-row justify-between bg-white">
+      {navigationList.map((item, idx) =>
+        segment === item.path ? (
+          <li
+            key={idx}
+            className="flex flex-col items-center justify-center gap-1 px-9 py-3 text-text-001"
+          >
+            <item.icon className="h-6 w-6" />
+            <p className="text-xs">{item.buttonText}</p>
+          </li>
+        ) : (
+          <Link href={item.path} key={idx}>
+            <li className="text-text-003 flex flex-col items-center justify-center gap-1 px-9 py-3">
+              <item.icon className="h-6 w-6" />
+              <p className="text-xs">{item.buttonText}</p>
+            </li>
+          </Link>
+        ),
+      )}
     </ul>
   );
 };
