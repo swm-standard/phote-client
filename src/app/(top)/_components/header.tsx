d19ce7fc4e38f96bf4bf385ed81fd2fe0426093f
송경호 <s07019@naver.com>
2024-07-29 01:31:53 +0900

    Refactor: refactor header login to use useSelectedLayoutSegment hook part of SWM-103 related to #29

--- src/app/(top)/_components/header.tsx
+++ src/app/(top)/_components/header.tsx
@@ -1,16 +1,23 @@
 'use client';
 import React from 'react';
-import { usePathname, useRouter } from 'next/navigation';
+import { useRouter, useSelectedLayoutSegment } from 'next/navigation';
 
-const getHeaderText = (pathname: string): string => {
-  if (pathname.includes('workbookDetail')) return '문제집 상세';
-  if (pathname.includes('createQuestion')) return '문제 생성';
-  return 'not found';
+const HeaderItems: Record<string, { text: string }> = {
+  createQuestion: {
+    text: '문제 생성',
+  },
+  workbookDetail: {
+    text: '문제집 상세',
+  },
+  none: {
+    text: '잘못된 주소',
+  },
 };
 
 const Header = () => {
   const router = useRouter();
-  const pathname = usePathname();
+  const segment = useSelectedLayoutSegment();
+  const headerItem = HeaderItems[segment || 'none'];
 
   const handleBackClick: React.MouseEventHandler<HTMLButtonElement> = () => {
     router.back();
@@ -19,7 +26,7 @@ const Header = () => {
   return (
     <div className="flex flex-row">
       <button onClick={handleBackClick}>go back</button>
-      <h1>{getHeaderText(pathname)}</h1>
+      <h1>{headerItem.text}</h1>
     </div>
   );
 };
