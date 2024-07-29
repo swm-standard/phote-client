fa9e273f304089557756708b8ee1baa690164dbc
송경호 <s07019@naver.com>
2024-07-29 19:50:49 +0900

    Refactor: change layout of workbook detail page part of SWM-118 related to #37

--- src/app/(top)/workbookDetail/[workbookId]/layout.tsx
+++ src/app/(top)/workbookDetail/[workbookId]/layout.tsx
@@ -1,18 +1,18 @@
-import React, { ReactNode } from 'react';
+import React, {ReactNode} from 'react';
 
 const Layout = ({
-  children,
-  dialog,
-}: {
-  children: ReactNode;
-  dialog: ReactNode;
+                    children,
+                    dialog,
+                }: {
+    children: ReactNode;
+    dialog: ReactNode;
 }) => {
-  return (
-    <div>
-      {children}
-      {dialog}
-    </div>
-  );
+    return (
+        <>
+            {children}
+            {dialog}
+        </>
+    );
 };
 
 export default Layout;
