3238998f6a74770a61f2b94dfb81b02042347f25
송경호 <s07019@naver.com>
2024-07-29 19:52:46 +0900

    Design: add icon prop to bar button and add bar button to workbook detail page part of SWM-118 related to #37

--- src/app/(top)/workbookDetail/[workbookId]/_components/workbook-question-area.tsx
+++ src/app/(top)/workbookDetail/[workbookId]/_components/workbook-question-area.tsx
@@ -1,43 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import { QuestionInWorkbook, Status } from '@/app/_lib/types';
-import { useParams } from 'next/navigation';
-import { BASE_URL } from '@/app/_lib/constants';
-import { Reorder } from 'framer-motion';
+import React, {useEffect, useState} from 'react';
+import {QuestionInWorkbook, Status} from '@/app/_lib/types';
+import {useParams} from 'next/navigation';
+import {BASE_URL} from '@/app/_lib/constants';
+import {Reorder} from 'framer-motion';
 import QuestionCards from '@/components/question-card';
+import Container from "@/components/container";
+import BarButton from "@/components/bar-button";
+import PlusIcon from "@/static/icons/plus-icon";
 
 const WorkbookQuestionArea = () => {
-  const [status, setStatus] = useState<Status>('loading');
-  const [questions, setQuestions] = useState<QuestionInWorkbook[]>([]);
-  const params = useParams<{ workbookId: string }>();
+    const [status, setStatus] = useState<Status>('loading');
+    const [questions, setQuestions] = useState<QuestionInWorkbook[]>([]);
+    const params = useParams<{ workbookId: string }>();
 
-  useEffect(() => {
-    fetch(`${BASE_URL}/workbook/questions/${params.workbookId}`)
-      .then((res) => res.json())
-      .then((res) => {
-        setQuestions(res);
-        setStatus('success');
-      })
-      .catch((err) => setStatus('error'));
-  }, []);
+    useEffect(() => {
+        fetch(`${BASE_URL}/workbook/questions/${params.workbookId}`)
+            .then((res) => res.json())
+            .then((res) => {
+                setQuestions(res);
+                setStatus('success');
+            })
+            .catch((err) => setStatus('error'));
+    }, []);
 
-  useEffect(() => {}, [questions]);
+    useEffect(() => {
+    }, [questions]);
 
-  if (status === 'loading') return <div>loading..</div>;
-  else if (status === 'error') return <div>Question Data fetch Error</div>;
+    if (status === 'loading') return <div>loading..</div>;
+    else if (status === 'error') return <div>Question Data fetch Error</div>;
 
-  return (
-    <div>
-      <Reorder.Group
-        axis="y"
-        onReorder={setQuestions}
-        values={questions}
-        layoutScroll
-        style={{ overflowY: 'scroll' }}
-      >
-        <QuestionCards questions={questions} allowSwap />
-      </Reorder.Group>
-    </div>
-  );
+    return (
+        <Container className='bg-white flex flex-col justify-between'>
+            <Reorder.Group
+                axis="y"
+                onReorder={setQuestions}
+                values={questions}
+                layoutScroll
+            >
+                <QuestionCards questions={questions} allowSwap/>
+            </Reorder.Group>
+            <div className="sticky bottom-4 my-4 px-4 w-full">
+                <BarButton
+                    Icon={PlusIcon}
+                    text="문제 등록"
+                    href={'/register-question'}
+                />
+            </div>
+        </Container>
+    );
 };
 
 export default WorkbookQuestionArea;
