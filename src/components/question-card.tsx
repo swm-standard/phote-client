2d71b0e8218b181a1487a2933d4c7996e0f492b2
송경호 <s07019@naver.com>
2024-07-29 19:54:14 +0900

    Design: publish question card part of SWM-118 related to #37

--- src/components/question-card.tsx
+++ src/components/question-card.tsx
@@ -1,174 +1,204 @@
-import React, { useEffect, useState } from 'react';
-import { Question, Status } from '@/app/_lib/types';
-import { DragControls, Reorder, useDragControls } from 'framer-motion';
-import { BASE_URL } from '@/app/_lib/constants';
-import { useParams } from 'next/navigation';
-import { useDebounce } from 'use-debounce';
+import React, {useEffect, useState} from 'react';
+import {Question, Status} from '@/app/_lib/types';
+import {DragControls, Reorder, useDragControls} from 'framer-motion';
+import {BASE_URL} from '@/app/_lib/constants';
+import {useParams} from 'next/navigation';
+import {useDebounce} from 'use-debounce';
+import AngleRightIcon from "@/static/icons/angle-right-icon";
+import TwoBarIcon from "@/static/icons/two-bar-icon";
+import AngleDownIcon from "@/static/icons/angle-down-icon";
+import InfoIcon from "@/static/icons/info-icon";
+import Image from "next/image";
+import dummy from '@/static/images/dummy-image-square.jpg'
+import NumberCircle from "@/components/number-circle";
 
 const ReorderWrapper = ({
-  question,
-  questionNumber,
-}: {
-  question: Question;
-  questionNumber: number;
+                            question,
+                            questionNumber,
+                        }: {
+    question: Question;
+    questionNumber: number;
 }) => {
-  const controls = useDragControls();
+    const controls = useDragControls();
 
-  return (
-    <Reorder.Item value={question} dragListener={false} dragControls={controls}>
-      <QuestionCard
-        question={question}
-        questionNumber={questionNumber}
-        controls={controls}
-        allowSwap
-      />
-    </Reorder.Item>
-  );
+    return (
+        <Reorder.Item value={question} dragListener={false} dragControls={controls}>
+            <QuestionCard
+                question={question}
+                questionNumber={questionNumber}
+                controls={controls}
+                allowSwap
+            />
+        </Reorder.Item>
+    );
 };
 
 const QuestionCard = ({
-  question,
-  questionNumber,
-  allowSwap = false,
-  controls,
-}: {
-  question: Question;
-  questionNumber: number;
-  allowSwap?: boolean;
-  controls?: DragControls;
+                          question,
+                          questionNumber,
+                          allowSwap = false,
+                          controls,
+                      }: {
+    question: Question;
+    questionNumber: number;
+    allowSwap?: boolean;
+    controls?: DragControls;
 }) => {
-  const [isExpanded, setExpanded] = useState<boolean>(false);
+    const [isExpanded, setExpanded] = useState<boolean>(false);
+    const category = question.category === 'ESSAY' ? "단답형" : "객관식"
 
-  const handleExpandToggleClick: React.MouseEventHandler<
-    HTMLButtonElement
-  > = () => {
-    setExpanded((prev) => !prev);
-  };
+    const handleExpandToggleClick: React.MouseEventHandler<
+        HTMLButtonElement
+    > = (e) => {
+        setExpanded((prev) => !prev);
+    };
 
-  const handleDragPointerDown: React.PointerEventHandler<HTMLButtonElement> = (
-    e,
-  ) => controls?.start(e);
+    const handleDragPointerDown: React.PointerEventHandler<HTMLButtonElement> = (
+        e,
+    ) => {
+        controls?.start(e)
+    };
 
-  if (!isExpanded) {
+    const handleClickBlock: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+        e.stopPropagation()
+    }
+
     return (
-      <div className="flex p-4 border-[1px]">
-        <button onClick={handleExpandToggleClick}>▶️</button>
-        <p>{`Q${questionNumber}`}</p>
-        <p>{question.category}</p>
-        {question.tags.map((tag, idx) => (
-          <p key={idx}>{tag}</p>
-        ))}
-        <p>{question.statement}</p>
-        {allowSwap && <button onPointerDown={handleDragPointerDown}>=</button>}
-      </div>
+        <button type='button' className='w-full' onClick={handleExpandToggleClick}>
+            <div
+                className="border-b-[1px] border-brand-gray-heavy">
+                <div
+                    className={`flex w-full justify-between bg-white gap-2 items-center p-4 ${isExpanded && "pb-0"}`}>
+                    <div>
+                        {isExpanded ? <AngleDownIcon className='w-4 h-4 text-text-001'/> :
+                            <AngleRightIcon className='w-4 h-4 text-text-001'/>
+                        }
+                    </div>
+                    <div className='flex-grow'>
+                        <div className='text-xs font-bold flex gap-1'>
+                            <span className='text-text-004'>{category}</span>
+                            {question.tags.map((tag, idx) => (
+                                <span key={idx} className='text-brand-blue-heavy'>{`#${tag}`}</span>
+                            ))}
+                        </div>
+                        {
+                            isExpanded ? (<p className='text-left font-normal text-base text-text-001'>
+                                    <span className='font-bold'>{`Q${questionNumber} `}</span>
+                                </p>
+                            ) : (<p className={`text-left font-normal text-base line-clamp-1 text-text-001`}>
+                                <span className='font-bold'>{`Q${questionNumber} `}</span>
+                                {question.statement}
+                            </p>)
+                        }
+                    </div>
+                    {(allowSwap && !isExpanded) &&
+                        <button onClick={handleClickBlock}
+                                onPointerDown={handleDragPointerDown}><TwoBarIcon
+                            className='w-4 h-4 text-text-003'/>
+                        </button>}
+                    {isExpanded && <button onClick={handleClickBlock}
+                                           onPointerDown={handleDragPointerDown}><InfoIcon
+                        className='w-4 h-4 text-text-003'/>
+                    </button>}
+                </div>
+                {
+                    isExpanded &&
+                    <div className='flex flex-col gap-4 py-4 px-10'>
+                        <div className='flex flex-col'>
+                            <span className='text-text-001 text-sm font-bold text-left'>[ 문제 설명 ]</span>
+                            <p className='text-left font-normal text-base text-text-001'>{question.statement}</p>
+                        </div>
+                        <Image src={dummy} alt='test'/>
+                        <div className='flex flex-col gap-1'>
+                            <span className='text-text-001 text-sm font-bold text-left'>[ 선택지 ]</span>
+                            <ul className='flex flex-col gap-1'>
+                                {
+                                    question.options.map((option, idx) => <li key={idx}
+                                                                              className='text-left flex items-center gap-2'>
+                                        <NumberCircle
+                                            number={idx + 1}/>{option.value}</li>)
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                }
+            </div>
+        </button>
     );
-  } else {
-    return (
-      <div className="flex flex-col p-4 border-[1px]">
-        <div className="flex">
-          <button onClick={handleExpandToggleClick}>🔽</button>
-          <p>{`Q${questionNumber}`}</p>
-          <p>{question.category}</p>
-          {question.tags.map((tag, idx) => (
-            <p key={idx}>{tag}</p>
-          ))}
-          <p>{question.statement}</p>
-          <button>ℹ️</button>
-        </div>
-        <img src={question.image} alt="사진 로드 실패" />
-        <div>문제 설명</div>
-        <p>{question.statement}</p>
-        <div>선택지</div>
-        <div className="flex">
-          {question.options.map((option, idx) => (
-            <p key={idx}>{`${idx + 1} ${option}`}</p>
-          ))}
-        </div>
-      </div>
-    );
-  }
 };
 
 const QuestionCards = ({
-  questions,
-  allowSwap = false,
-}: {
-  // questions: QuestionInWorkbook[];
-  questions: Question[];
-  allowSwap?: boolean;
+                           questions,
+                           allowSwap = false,
+                       }: {
+    questions: Question[];
+    allowSwap?: boolean;
 }) => {
-  const [status, setStatus] = useState<Status>('loading');
-  const [countApiCalls, setCountApiCalls] = useState<number>(0);
-  const params = useParams<{ workbookId: string }>();
-  const [debouncedQuestions] = useDebounce(questions, 2000);
+    const [status, setStatus] = useState<Status>('loading');
+    const [countApiCalls, setCountApiCalls] = useState<number>(0);
+    const params = useParams<{ workbookId: string }>();
+    const [debouncedQuestions] = useDebounce(questions, 2000);
 
-  const updateQuestionSequence = async () => {
-    if (!allowSwap) return;
-    if (countApiCalls === 0) {
-      setCountApiCalls((prev) => prev + 1);
-      return;
-    }
+    const updateQuestionSequence = async () => {
+        if (!allowSwap) return;
+        if (countApiCalls === 0) {
+            setCountApiCalls((prev) => prev + 1);
+            return;
+        }
 
-    const requestData = questions.map((ques, idx) => {
-      return { id: ques.id, sequence: idx + 1 };
-    });
+        const requestData = questions.map((ques, idx) => {
+            return {id: ques.id, sequence: idx + 1};
+        });
 
-    try {
-      const response = await fetch(
-        `${BASE_URL}/workbook/question-sequence/${params.workbookId}`,
-        {
-          method: 'PATCH',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(requestData),
-        },
-      );
+        try {
+            const response = await fetch(
+                `${BASE_URL}/workbook/question-sequence/${params.workbookId}`,
+                {
+                    method: 'PATCH',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(requestData),
+                },
+            );
 
-      const data = await response.json();
-      setStatus('success');
-    } catch (err) {
-      setStatus('error');
-    } finally {
-      setCountApiCalls((prev) => prev + 1);
-    }
-  };
+            const data = await response.json();
+            setStatus('success');
+        } catch (err) {
+            setStatus('error');
+        } finally {
+            setCountApiCalls((prev) => prev + 1);
+        }
+    };
 
-  useEffect(() => {
-    (async () => {
-      await updateQuestionSequence();
-    })();
-  }, [debouncedQuestions]);
+    useEffect(() => {
+        (async () => {
+            await updateQuestionSequence();
+        })();
+    }, [debouncedQuestions]);
 
-  return (
-    <div className="flex flex-col gap-4 p-4">
-      {allowSwap && (
-        <>
-          <p>Debounce Check</p>
-          <p>{`question_swap_status:${status}`}</p>
-          <p>{`API calls : ${countApiCalls}`}</p>
-        </>
-      )}
-      {questions.map((ques, idx) => {
-        if (allowSwap)
-          return (
-            <ReorderWrapper
-              key={ques.id}
-              question={ques}
-              questionNumber={idx + 1}
-            />
-          );
-        else
-          return (
-            <QuestionCard
-              key={ques.id}
-              question={ques}
-              questionNumber={idx + 1}
-            />
-          );
-      })}
-    </div>
-  );
+    return (
+        <div className="flex flex-col">
+            {questions.map((ques, idx) => {
+                if (allowSwap)
+                    return (
+                        <ReorderWrapper
+                            key={ques.id}
+                            question={ques}
+                            questionNumber={idx + 1}
+                        />
+                    );
+                else
+                    return (
+                        <QuestionCard
+                            key={ques.id}
+                            question={ques}
+                            questionNumber={idx + 1}
+                        />
+                    );
+            })}
+        </div>
+    );
 };
 
 export default QuestionCards;
