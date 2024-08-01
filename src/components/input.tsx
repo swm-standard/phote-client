f214020c297f7deabe3c6cee3b38b9d38730675e
송경호 <s07019@naver.com>
2024-08-01 11:59:29 +0900

    Feat: implement common building block components used in form part of SWM-120 related to #41

---
+++ src/components/input.tsx
@@ -0,0 +1,61 @@
+import React, { RefObject, useState } from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form';
+import { cva, VariantProps } from 'class-variance-authority';
+import { cn } from '@/lib/utils';
+
+const variants = cva(
+  'flex items-center rounded-lg border-[1px] p-2 text-sm font-normal text-text-001 placeholder-text-003 border-brand-gray-light',
+  {
+    variants: {
+      state: {
+        empty: 'border-brand-gray-heavy',
+        focused: 'border-brand-blue-heavy',
+        filled: 'bg-brand-gray-light',
+      },
+    },
+    defaultVariants: {
+      state: 'empty',
+    },
+  },
+);
+
+type InputProps = Omit<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  'className'
+> & {
+  className?: string;
+  register: UseFormRegisterReturn;
+  textLength: number;
+  ref?: RefObject<HTMLInputElement>;
+} & VariantProps<typeof variants>;
+
+const Input = ({
+  className,
+  register,
+  state,
+  textLength,
+  ...props
+}: InputProps) => {
+  const [focused, setFocused] = useState<boolean>(false);
+
+  return (
+    <div
+      className={cn(
+        variants({ state }),
+        className,
+        focused ? 'border-2 border-brand-blue-heavy bg-white p-[7px]' : '',
+      )}
+    >
+      <input
+        {...register}
+        {...props}
+        className="flex-grow bg-inherit"
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+      />
+      <p className="text-xs font-medium text-text-003">{`${textLength} / ${props.maxLength}`}</p>
+    </div>
+  );
+};
+
+export default Input;
---
+++ src/components/lined-input.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form';
+import { cn } from '@/lib/utils';
+
+// const variants = cva();
+
+type InputProps = Omit<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  'className'
+> & {
+  className?: string;
+  register: UseFormRegisterReturn;
+  textLength: number;
+};
+// & VariantProps<typeof variants>;
+
+const LinedInput = ({
+  className,
+  register,
+  // state,
+  textLength,
+  ...props
+}: InputProps) => {
+  const [focused, setFocused] = useState<boolean>(false);
+
+  return (
+    <div
+      className={cn(
+        'flex items-center',
+        focused ? 'border-b-[1px] border-brand-blue-heavy' : 'pb-[1px]',
+      )}
+    >
+      <input
+        {...register}
+        {...props}
+        className={cn(
+          'flex-grow text-base font-medium text-text-001 placeholder-text-003 placeholder:font-extralight',
+          className,
+        )}
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+      />
+      {focused && (
+        <p className="text-xs font-medium text-text-003">{`${textLength} / ${props.maxLength}`}</p>
+      )}
+    </div>
+  );
+};
+
+export default LinedInput;
