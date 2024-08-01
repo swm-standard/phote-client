2f1baddcded9574db91202e056f5124f37acaeb1
송경호 <s07019@naver.com>
2024-08-01 12:11:28 +0900

    Feat: add UI design and implement related feture of add extra info section part of SWM-120 related to #41

--- src/app/(top)/createQuestion/_components/content/add-extra-info.tsx
+++ src/app/(top)/createQuestion/_components/content/add-extra-info.tsx
@@ -1,47 +1,103 @@
-import React, { useState } from 'react';
-import { UseFormRegister } from 'react-hook-form';
+import React from 'react';
+import {
+  useForm,
+  UseFormRegister,
+  UseFormSetValue,
+  UseFormWatch,
+} from 'react-hook-form';
 import { QuestionBase } from '@/app/_lib/types';
+import Legend from '@/components/legend';
+import Input from '@/components/input';
+import Textarea from '@/components/textarea';
 
 export const AddExtraInfo = ({
   register,
-  tags,
-  addTag,
-  removeTag,
+  watch,
+  setValue,
 }: {
   register: UseFormRegister<QuestionBase>;
-  tags: string[];
-  addTag: (tag: string) => void;
-  removeTag: (tag: string) => void;
+  watch: UseFormWatch<QuestionBase>;
+  setValue: UseFormSetValue<QuestionBase>;
 }) => {
-  const [value, setValue] = useState<string>('');
+  const values = watch();
+  const {
+    register: tagRegister,
+    watch: tagWatch,
+    reset,
+  } = useForm<{
+    tagInput: string;
+  }>({
+    defaultValues: {
+      tagInput: '',
+    },
+  });
+  const tagValue = tagWatch();
+
+  const handleEnterDown: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
+    if (e.key !== 'Enter') return;
+    setValue('tags', [...values.tags, tagValue.tagInput]);
+    reset();
+  };
 
   return (
-    <form>
+    <div className="flex flex-col gap-6">
       <fieldset>
-        <legend>정답</legend>
-        <input {...register('answer')} />
+        <Legend required className="mb-2">
+          정답
+        </Legend>
+        <Input
+          placeholder="정답을 입력해주세요."
+          register={register('answer')}
+          state={values.answer?.length > 0 ? 'filled' : 'empty'}
+          textLength={values.answer?.length}
+          maxLength={25}
+        />
       </fieldset>
       <fieldset>
-        <legend>태그</legend>
-        <input onChange={(e) => setValue(e.target.value)} />
-        <button type="button" onClick={() => addTag(value)}>
-          추가
-        </button>
-        <div className="flex gap-4">
-          {tags.map((tag, idx) => (
-            <div key={idx} className="border-2">
-              <span>{tag}</span>
-              <button type="button" onClick={() => removeTag(tag)}>
-                X
-              </button>
-            </div>
+        <Legend className="mb-2">태그</Legend>
+        <Input
+          register={tagRegister('tagInput')}
+          textLength={tagValue.tagInput?.length}
+          maxLength={25}
+          onKeyDown={handleEnterDown}
+        />
+        <div className="mt-2 flex gap-4">
+          {values.tags?.map((tag, idx) => (
+            <Tag
+              tag={tag}
+              key={idx}
+              removeTag={() => {
+                setValue(
+                  'tags',
+                  values.tags.filter((t) => t !== tag),
+                );
+              }}
+            />
           ))}
         </div>
       </fieldset>
       <fieldset>
-        <legend>메모</legend>
-        <textarea {...register('memo')} />
+        <Legend className="mb-2">메모</Legend>
+        <Textarea
+          className="h-32 w-full"
+          register={register('memo')}
+          placeholder="자유롭게 작성하세요."
+          state={values.memo?.length > 0 ? 'filled' : 'empty'}
+          textLength={values.memo?.length}
+          maxLength={100}
+        />
       </fieldset>
-    </form>
+    </div>
+  );
+};
+
+const Tag = ({ tag, removeTag }: { tag: string; removeTag: () => void }) => {
+  return (
+    <button onClick={() => removeTag()}>
+      <div className="flex items-center gap-1 rounded-full bg-brand-white px-2 py-1">
+        <p className="text-sm font-normal text-brand-blue-heavy"># {tag}</p>
+        <p className="text-text-004">x</p>
+      </div>
+    </button>
   );
 };
