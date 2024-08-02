de2cb23cf6d5893716ce8a9095b50331bc5332b6
송경호 <s07019@naver.com>
2024-08-02 16:28:19 +0900

    Feat: move question fetch logic to searched-questions.tsx part of SWM-128 related to #43

--- src/app/(nav)/question/page.tsx
+++ src/app/(nav)/question/page.tsx
@@ -1,54 +1,26 @@
 'use client';
 
 import SearchHeader from '@/app/(nav)/question/search-header';
+import React from 'react';
+import Container from '@/components/container';
 import SearchedQuestions from '@/app/(nav)/question/searched-questions';
-import React, { useEffect, useState } from 'react';
-import { Question, Status } from '@/app/_lib/types';
-import { BASE_URL } from '@/app/_lib/constants';
 
-const Page = ({
-  searchParams,
-}: {
-  searchParams?: {
-    tags?: string;
-    keywords?: string;
-  };
-}) => {
-  const [status, setStatus] = useState<Status>('loading');
-  const [searchedQuestions, setSearchedQuestions] = useState<Question[]>([]);
-
-  const callSearchQuestionsAPI = async () => {
-    const params = new URLSearchParams();
-    searchParams?.tags && params.append('tags', searchParams.tags);
-    searchParams?.keywords && params.append('keywords', searchParams.keywords);
-
-    try {
-      const response = await fetch(
-        `${BASE_URL}/questions?${params.toString()}`,
-      );
-
-      const data = await response.json();
-
-      setSearchedQuestions(data);
-      setStatus('success');
-    } catch (error) {
-      setStatus('error');
-    }
-  };
-
-  useEffect(() => {
-    (async () => {
-      await callSearchQuestionsAPI();
-    })();
-  }, [searchParams]);
-
-  if (status === 'loading') return <div>loading</div>;
-  else if (status === 'error') return <div>error</div>;
+const Page = () => {
   return (
-    <div>
-      <SearchHeader searchedQuestionNumber={searchedQuestions.length} />
-      <SearchedQuestions questions={searchedQuestions} />
-    </div>
+    <Container className="flex flex-col overflow-scroll">
+      <section className="px-4 pt-6">
+        <p className="text-2xl font-bold text-text-001">문제 검색하기</p>
+        <p className="mt-1 text-base font-normal text-text-002">
+          # 을 통해 태그 검색도 가능해요 😃
+        </p>
+      </section>
+      <section className="sticky top-0 bg-app-bg px-4 py-5">
+        <SearchHeader />
+      </section>
+      <section className="flex-grow">
+        <SearchedQuestions />
+      </section>
+    </Container>
   );
 };
 
