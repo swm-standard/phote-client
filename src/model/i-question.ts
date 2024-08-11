4f13e3cf43eb2b2db2940ad4194ca5dc472cd263
송경호 <s07019@naver.com>
2024-08-11 15:17:17 +0900

    Feat: add types for progress and question creation part of SWM-154 related to #59

--- src/model/i-question.ts
+++ src/model/i-question.ts
@@ -2,7 +2,7 @@ export type IQuestion = {
   questionId: string;
   statement: string;
   options: string[];
-  image: string;
+  image?: string;
   category: IQuestionCategory;
   tags: IQuestionTag[];
 };
@@ -12,4 +12,28 @@ export type IQuestionTag = {
   name: string;
   createdAt: string;
   modifiedAt: string;
+};
+
+export type ICreateQuestion = {
+  statement: string;
+  image: string;
+  category: IQuestionCategory;
+  options: Option[];
+  answer: string;
+  tags: string[];
+  memo: string;
+};
+
+type Option = {
+  value: string;
+};
+
+export const EmptyCreateQuestion: ICreateQuestion = {
+  statement: '',
+  image: '',
+  category: 'MULTIPLE',
+  options: [],
+  answer: '',
+  tags: [],
+  memo: '',
 };
---
+++ src/model/i-step.ts
@@ -0,0 +1,1 @@
+export type IStep = 1 | 2 | 3;
