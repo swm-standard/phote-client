9a15b910dbd8cfb971a695d31d4ca8a50c5253c4
송경호 <s07019@naver.com>
2024-08-11 15:21:23 +0900

    Chore: remove intercepting route part of SWM-154 related to #59

--- src/app/(after-login)/(top)/createQuestion/intercepted/cancel-alert-dialog/page.tsx
+++
@@ -1,7 +0,0 @@
-import CancelAlertDialog from '@/app/(after-login)/(top)/createQuestion/_components/cancel-alert-dialog';
-
-const Page = () => {
-  return <CancelAlertDialog />;
-};
-
-export default Page;
