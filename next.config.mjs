540a3423ae6a15c69714b3df9785807de60c88e0
송경호 <s07019@naver.com>
2024-08-23 11:43:41 +0900

    Chore: add s3 domain at next.config.mjs part of SWM-164 related to #67

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false, // auth code check
+    images: {
+        domains: ['phote-be-bucket.s3.amazonaws.com'],
+    },
 };
 
 export default nextConfig;
