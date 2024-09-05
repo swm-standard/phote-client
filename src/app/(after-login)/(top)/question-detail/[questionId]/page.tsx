470ba8cf39d0be3ea0291e5b4879145bfc246705
송경호 <s07019@naver.com>
2024-09-05 18:55:17 +0900

    Feat: style change for question detail

--- src/app/(after-login)/(top)/question-detail/[questionId]/page.tsx
+++ src/app/(after-login)/(top)/question-detail/[questionId]/page.tsx
@@ -32,104 +32,108 @@ const Page = ({ params }: { params: { questionId: string } }) => {
     router.back();
   };
 
-  console.log(data);
   if (isFetching) return <Loading />;
-  else if (isError) return <div>error</div>;
+  else if (isError || mutation.isError) return <div>error</div>;
   else
     return (
-      <Container className="bg-white px-6 pt-4">
-        <section className="flex flex-col gap-6">
-          <fieldset>
-            <Legend required className="mb-2 text-sm">
-              문제설명
-            </Legend>
-            <textarea
-              rows={3}
-              className="w-full rounded-lg border-[1px] border-brand-gray-heavy p-2 text-sm font-normal text-text-001"
-              value={data.statement}
-              readOnly
-            />
-          </fieldset>
-
-          {data.image && (
-            <fieldset>
-              <Legend className="mb-2 text-sm">문제 그림</Legend>
-              <div className="relative mx-auto aspect-square w-full overflow-hidden rounded-lg">
-                <Image src={data.image} fill alt="문제" />
-              </div>
-            </fieldset>
-          )}
-
-          <fieldset>
-            <Legend className="mb-2 text-sm" required>
-              문제 유형
-            </Legend>
-            <div className="flex gap-2">
-              <SquareButton
-                theme={data.category === 'MULTIPLE' ? 'lightblue' : 'lightgray'}
-                className="flex-grow py-2"
-              >
-                객관식
-              </SquareButton>
-              <SquareButton
-                theme={data.category === 'ESSAY' ? 'lightblue' : 'lightgray'}
-                className="flex-grow py-2"
-              >
-                단답형
-              </SquareButton>
-            </div>
-          </fieldset>
-
-          {data.category === 'MULTIPLE' && (
+      <>
+        {mutation.isPending ? <Loading /> : null}
+        <Container className="bg-white px-6 pt-4">
+          <section className="flex flex-col gap-6">
             <fieldset>
               <Legend required className="mb-2 text-sm">
-                선택지
+                문제설명
               </Legend>
-              <ul className="flex flex-col gap-4">
-                {data.options.map((field: string, idx: number) => (
-                  <li key={idx} className="flex items-center gap-4">
-                    <NumberCircle number={idx + 1} className="mb-[1px]" />
-                    <section className="flex-grow">{field}</section>
-                  </li>
-                ))}
-              </ul>
+              <textarea
+                rows={3}
+                className="w-full rounded-lg border-[1px] border-brand-gray-heavy p-2 text-sm font-normal text-text-001"
+                value={data.statement}
+                readOnly
+              />
             </fieldset>
-          )}
 
-          <fieldset>
-            <Legend required className="mb-2 text-sm">
-              정답
-            </Legend>
-            {data.answer}
-          </fieldset>
-          <fieldset>
-            <Legend className="mb-2 text-sm">태그</Legend>
-            <div className="flex gap-4">
-              {data.tags.map((tag: { name: string }, idx: number) => (
-                <span key={idx}>{tag.name}</span>
-              ))}
-            </div>
-          </fieldset>
-          <fieldset>
-            <Legend className="mb-2 text-sm">메모</Legend>
-            <textarea
-              className="w-full rounded-lg border-[1px] border-brand-gray-heavy p-2 text-sm font-normal text-text-001"
-              value={data.memo}
-              readOnly
-            />
-          </fieldset>
-        </section>
+            {data.image && (
+              <fieldset>
+                <Legend className="mb-2 text-sm">문제 그림</Legend>
+                <div className="relative mx-auto aspect-square w-full overflow-hidden rounded-lg">
+                  <Image src={data.image} fill alt="문제" />
+                </div>
+              </fieldset>
+            )}
 
-        <div className="sticky bottom-0 w-full bg-transparent py-4">
-          <BarButton
-            icon={TrashCanIcon}
-            barButtonType="button"
-            onClick={handleDeleteClick}
-          >
-            문제 삭제
-          </BarButton>
-        </div>
-      </Container>
+            <fieldset>
+              <Legend className="mb-2 text-sm" required>
+                문제 유형
+              </Legend>
+              <div className="flex gap-2">
+                <SquareButton
+                  theme={
+                    data.category === 'MULTIPLE' ? 'lightblue' : 'lightgray'
+                  }
+                  className="flex-grow py-2"
+                >
+                  객관식
+                </SquareButton>
+                <SquareButton
+                  theme={data.category === 'ESSAY' ? 'lightblue' : 'lightgray'}
+                  className="flex-grow py-2"
+                >
+                  단답형
+                </SquareButton>
+              </div>
+            </fieldset>
+
+            {data.category === 'MULTIPLE' && (
+              <fieldset>
+                <Legend required className="mb-2 text-sm">
+                  선택지
+                </Legend>
+                <ul className="flex flex-col gap-4">
+                  {data.options.map((field: string, idx: number) => (
+                    <li key={idx} className="flex items-center gap-4">
+                      <NumberCircle number={idx + 1} className="mb-[1px]" />
+                      <section className="flex-grow">{field}</section>
+                    </li>
+                  ))}
+                </ul>
+              </fieldset>
+            )}
+
+            <fieldset>
+              <Legend required className="mb-2 text-sm">
+                정답
+              </Legend>
+              {data.answer}
+            </fieldset>
+            <fieldset>
+              <Legend className="mb-2 text-sm">태그</Legend>
+              <div className="flex gap-4">
+                {data.tags.map((tag: { name: string }, idx: number) => (
+                  <span key={idx}>{tag.name}</span>
+                ))}
+              </div>
+            </fieldset>
+            <fieldset>
+              <Legend className="mb-2 text-sm">메모</Legend>
+              <textarea
+                className="w-full rounded-lg border-[1px] border-brand-gray-heavy p-2 text-sm font-normal text-text-001"
+                value={data.memo}
+                readOnly
+              />
+            </fieldset>
+          </section>
+
+          <div className="sticky bottom-0 w-full bg-transparent py-4">
+            <BarButton
+              icon={TrashCanIcon}
+              barButtonType="button"
+              onClick={handleDeleteClick}
+            >
+              문제 삭제
+            </BarButton>
+          </div>
+        </Container>
+      </>
     );
 };
 
