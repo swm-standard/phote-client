5aaa938dec837c0174754361ba643801a119889d
송경호 <s07019@naver.com>
2024-09-12 12:28:11 +0900

    Feat: add float point fix

--- src/app/(after-login)/(top)/workbook-detail/[workbookId]/_components/exam-list/exam-cards.tsx
+++ src/app/(after-login)/(top)/workbook-detail/[workbookId]/_components/exam-list/exam-cards.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { IExam } from '@/model/i-exam';
 import Link from 'next/link';
 import Container from '@/components/container';
+import { fixFloatPoint } from '@/lib/utils';
 
 export const ExamCard = ({ exam }: { exam: IExam }) => {
   return (
     <Link
       href={`/exam-detail/${exam.examId}`}
-      className="flex items-center justify-center gap-20 border-b-[1px] border-brand-gray-heavy bg-inherit bg-white px-4 py-4"
+      className="flex justify-between border-b-[1px] border-brand-gray-heavy bg-inherit bg-white px-8 py-4"
     >
       <WordChunk
         label="총 점수"
-        value={`${(exam.totalCorrect / exam.totalQuantity) * 100}점`}
+        value={`${fixFloatPoint((exam.totalCorrect / exam.totalQuantity) * 100)}점`}
       />
       <WordChunk label="걸린 시간" value={`${exam.time}분`} />
       <WordChunk label="시험" value={`${exam.sequence}번째 시험`} />
@@ -27,7 +28,7 @@ export const WordChunk = ({
   value: string;
 }) => {
   return (
-    <div className="flex flex-col items-start justify-center">
+    <div className="flex flex-1 flex-col items-start justify-center">
       <p className="text-xs font-normal text-text-003">{label}</p>
       <p className="text-base font-semibold text-text-001">{value}</p>
     </div>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function fixFloatPoint(num: number) {
+  return Number(num.toFixed(2));
+}
