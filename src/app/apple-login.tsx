4eb553f8ab35b7d356ef82344ac1666dc81941ae
송경호 <s07019@naver.com>
2024-09-19 12:12:42 +0900

    Fix: apple login

---
+++ src/app/apple-login.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import Script from 'next/script';
+
+interface AppleIDSignInOnFailure extends Event {
+  detail: {
+    error: Error;
+  };
+}
+
+const AppleLogin = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const handleAppleScriptLoad = () => {
+    // @ts-ignore
+    const apple = window['AppleID'];
+    if (!apple) return;
+
+    apple.auth.init({
+      clientId: process.env['NEXT_PUBLIC_APPLE_API_KEY'],
+      scope: 'name email',
+      redirectURI: `${window.location.origin}${window.location.pathname}`,
+      state: 'siginin',
+      usePopup: true,
+    });
+    // 애플로 로그인 성공
+
+    document.addEventListener(
+      'AppleIDSignInOnSuccess',
+      handleAppleCredentialResponse,
+    );
+    // 애플로 로그인 실패
+    document.addEventListener('AppleIDSignOnFailure', () =>
+      console.error('apple 로그인 실패'),
+    );
+
+    setIsLoaded(true);
+  };
+
+  // @ts-ignore
+  const handleAppleCredentialResponse = async (e) => {
+    console.log(e.detail.authorization);
+  };
+
+  return (
+    <>
+      <Script
+        src="https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/ko_KR/appleid.auth.js"
+        onLoad={handleAppleScriptLoad}
+      />
+      <div
+        className="button-apple"
+        style={{ visibility: isLoaded ? 'visible' : 'hidden' }}
+      >
+        <div
+          id="appleid-signin"
+          data-color="black"
+          data-type="continue"
+          data-mode="left-align"
+          data-width="323"
+          data-height="36"
+          data-border="true"
+          data-border-radius="50"
+          data-logo-size="medium"
+          data-logo-position="15"
+          data-label-position="115"
+          style={{
+            width: 323,
+            height: 36,
+          }}
+        />
+      </div>
+    </>
+  );
+};
+
+export default AppleLogin;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,69 +5,72 @@ import React from 'react';
 import Image from 'next/image';
 import kakao from '@/static/images/kakao-login.png';
 import google from '@/static/images/google-login.png';
-import apple from '@/static/images/apple-login.png';
 import logo_character from '@/static/images/logo-character.png';
 import logo_typo from '@/static/images/logo-typo.png';
 import Container from '@/components/container';
+import AppleLogin from '@/app/apple-login';
 
 const Page = () => {
   return (
-    <Container className="flex flex-col">
-      <section className="mx-32 flex flex-grow flex-col items-center justify-center gap-2">
-        <div className="relative w-full">
-          <Image
-            src={logo_character}
-            alt=""
-            layout="responsive"
-            objectFit="contain"
-          />
-        </div>
-        <div className="relative w-full">
-          <Image
-            src={logo_typo}
-            alt="포테"
-            layout="responsive"
-            objectFit="contain"
-          />
-        </div>
-      </section>
+    <>
+      <Container className="flex flex-col">
+        <section className="mx-32 flex flex-grow flex-col items-center justify-center gap-2">
+          <div className="relative w-full">
+            <Image
+              src={logo_character}
+              alt=""
+              layout="responsive"
+              objectFit="contain"
+            />
+          </div>
+          <div className="relative w-full">
+            <Image
+              src={logo_typo}
+              alt="포테"
+              layout="responsive"
+              objectFit="contain"
+            />
+          </div>
+        </section>
 
-      <section className="mb-10 flex w-full flex-col items-center justify-center gap-4 px-4">
-        <Link
-          href={`https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env['NEXT_PUBLIC_GOOGLE_API_KEY']}&response_type=code&redirect_uri=${process.env['NEXT_PUBLIC_LOGIN_REDIRECT_URL']}/redirect/google&scope=https://www.googleapis.com/auth/userinfo.email`}
-          className="relative h-fit w-full"
-        >
-          <Image
-            src={google}
-            alt="구글 로그인"
-            layout="responsive"
-            objectFit="contain"
-          />
-        </Link>
-        <Link
-          href={`https://kauth.kakao.com/oauth/authorize?client_id=${process.env['NEXT_PUBLIC_KAKAO_API_KEY']}&redirect_uri=${process.env['NEXT_PUBLIC_LOGIN_REDIRECT_URL']}/redirect/kakao&response_type=code`}
-          className="relative w-full"
-        >
-          <Image
-            src={kakao}
-            alt="카카오 로그인"
-            layout="responsive"
-            objectFit="contain"
-          />
-        </Link>
-        <Link
-          href={`https://appleid.apple.com/auth/authorize?client_id=${process.env['NEXT_PUBLIC_APPLE_API_KEY']}&redirect_uri=${process.env['NEXT_PUBLIC_LOGIN_REDIRECT_URL']}/redirect/apple&response_type=code%20id_token&scope=name%20email&response_mode=form_post`}
-          className="relative w-full"
-        >
-          <Image
-            src={apple}
-            alt="애플 로그인"
-            layout="responsive"
-            objectFit="contain"
-          />
-        </Link>
-      </section>
-    </Container>
+        <section className="mb-10 flex w-full flex-col items-center justify-center gap-4 px-4">
+          <Link
+            href={`https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env['NEXT_PUBLIC_GOOGLE_API_KEY']}&response_type=code&redirect_uri=${process.env['NEXT_PUBLIC_LOGIN_REDIRECT_URL']}/redirect/google&scope=https://www.googleapis.com/auth/userinfo.email`}
+            className="relative h-fit w-full"
+          >
+            <Image
+              src={google}
+              alt="구글 로그인"
+              layout="responsive"
+              objectFit="contain"
+            />
+          </Link>
+          <Link
+            href={`https://kauth.kakao.com/oauth/authorize?client_id=${process.env['NEXT_PUBLIC_KAKAO_API_KEY']}&redirect_uri=${process.env['NEXT_PUBLIC_LOGIN_REDIRECT_URL']}/redirect/kakao&response_type=code`}
+            className="relative w-full"
+          >
+            <Image
+              src={kakao}
+              alt="카카오 로그인"
+              layout="responsive"
+              objectFit="contain"
+            />
+          </Link>
+          <AppleLogin />
+          {/*<Link*/}
+          {/*  href={`https://appleid.apple.com/auth/authorize?client_id=${process.env['NEXT_PUBLIC_APPLE_API_KEY']}&redirect_uri=${process.env['NEXT_PUBLIC_LOGIN_REDIRECT_URL']}/redirect/apple&response_type=code%20id_token&scope=name%20email&response_mode=form_post`}*/}
+          {/*  className="relative w-full"*/}
+          {/*>*/}
+          {/*  <Image*/}
+          {/*    src={apple}*/}
+          {/*    alt="애플 로그인"*/}
+          {/*    layout="responsive"*/}
+          {/*    objectFit="contain"*/}
+          {/*  />*/}
+          {/*</Link>*/}
+        </section>
+      </Container>
+    </>
   );
 };
 
