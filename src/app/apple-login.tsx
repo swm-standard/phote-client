8606d21e7b0ec5a8e30709863b261f8def041bc1
송경호 <s07019@naver.com>
2024-09-19 17:42:58 +0900

    Feat: hide apple button

--- src/app/apple-login.tsx
+++ src/app/apple-login.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Script from 'next/script';
 import { useRouter } from 'next/navigation';
+import apple from '@/static/images/apple-login.png';
+import Image from 'next/image';
 
 // interface AppleIDSignInOnFailure extends Event {
 //   detail: {
@@ -44,6 +46,14 @@ const AppleLogin = () => {
     router.push(`/redirect/apple?code=${e.detail.authorization.code}`);
   };
 
+  const appleButtonRef = useRef(null);
+  const handleCustomButtonClick = () => {
+    if (appleButtonRef.current) {
+      // @ts-expect-error fix apple login
+      appleButtonRef.current.click(); // 숨겨진 Apple 로그인 버튼을 클릭
+    }
+  };
+
   return (
     <>
       <Script
@@ -51,25 +61,26 @@ const AppleLogin = () => {
         onLoad={handleAppleScriptLoad}
       />
       <div
-        className="button-apple"
+        className="w-full"
         style={{ visibility: isLoaded ? 'visible' : 'hidden' }}
       >
         <div
+          ref={appleButtonRef}
           id="appleid-signin"
+          style={{ display: 'none' }}
           data-color="black"
           data-type="continue"
           data-mode="left-align"
           data-width="323"
           data-height="36"
-          data-border="true"
-          data-border-radius="50"
-          data-logo-size="medium"
-          data-logo-position="15"
-          data-label-position="115"
-          style={{
-            width: 323,
-            height: 36,
-          }}
+        />
+        <Image
+          onClick={handleCustomButtonClick}
+          className="cursor-pointer"
+          src={apple}
+          alt="애플 로그인"
+          layout="responsive"
+          objectFit="contain"
         />
       </div>
     </>
